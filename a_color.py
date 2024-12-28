a9a5f21e134822923b722a561ee6910fc8530731
contactlucky12329 <contactlucky12329@gmail.com>
2024-12-28 20:13:44 +0530

    Commit

--- a_color.py
+++ a_color.py
@@ -1,53 +1,96 @@
 import streamlit as st
-
+import pandas as pd
+import database_master as db_mast
 st.title("Lakhwinder")
 
 
 response = st.sidebar.selectbox("Take your position",("BUY","SELL" ))
 
+def fetch_batch_counter(filename="default_name_batch_counter.txt"):
+    try:
+        with open(filename, 'r') as file:
+            counter = file.read().strip()
+            return int(counter) if counter else 1
+    except FileNotFoundError:
+        print(f"{filename} not found. Starting from batch 1.")
+        return 1
+    except ValueError:
+        print(f"Nothing in {filename}. Starting from batch 1.")
+        return 1
 
-col1, col2, col3 = st.columns(3)
 
+def fetch_data_from_database():
+    batch_counter = fetch_batch_counter("batch_counter_websocket.txt")
+    print(f"Batch counter running = {batch_counter}")
+    query = f"""  WITH RankedData AS (
+        SELECT
+            [symbol],
+            [ltp],
+                [sno],
+                [rank],
+            [low_price],
+            [high_price],
+            [upper_ckt],
+            [lower_ckt],
+            [open_price],
+            [prev_close_price],
+            [chp],
+            [vol_traded_today],
+            [last_traded_time],
+            [exch_feed_time],
+            [inserted_at],
+            [batch],
+            ROW_NUMBER() OVER (PARTITION BY [symbol] ORDER BY ABS(chp) DESC) AS rn
+        FROM [algoMaster].[dbo].[stock_live_data]
+        WHERE CAST(inserted_at AS DATE) = '2024-12-26'
+          AND batch = 1 and ltp > 30
+        )
+            SELECT TOP (20)
+                [symbol],
+                [ltp],
+                        [sno],
+                        [rank],
+                [low_price],
+                [high_price],
+                [upper_ckt],
+                [lower_ckt],
+                [open_price],
+                [prev_close_price],
+                [chp],
+                [vol_traded_today],
+                [last_traded_time],
+                [exch_feed_time],
+                [inserted_at],
+                [batch]
+            FROM RankedData
+            WHERE rn = 1
+            ORDER BY  ABS(chp) DESC
+          """
+    print(query)
+    data, columns = db_mast.get_data_algoMaster1(query)
+    # print(data)
+    if not data or not columns:
+        print("No data found, may be date changed (inserted_at not matching with given date)")
+        return []
 
-with col1:
-    label3 = st.text("This is a label")
+    try:
+        df = pd.DataFrame.from_records(data, columns=columns)
+        df['sno'] = range(1, len(df) + 1)  # Reset 'sno'
+        df = df.sort_values(by='sno', ascending=True)
+        stock_data1 = df.to_dict(orient='records')
+        # st.write(stock_data1)
+        return stock_data1
+    except Exception as e:
+        print(f"Error loading data into DataFrame: {e}")
+        return []
+def make_clickable(link):
+    # Target="_blank" opens the link in a new tab
+    return f'<a target="_blank" href="{link}">Open Chart</a>'
 
-with col2:
-    label4 = st.text("This is a label")
+#
+if st.button("Fetch Data"):
+    data = fetch_data_from_database()
+    if data is not None:
+        # data['Open_Chart'] = data['Chart_Link_Column'].apply(make_clickable)
 
-with col3:
-    label5 = st.text("This is a label")
-
-button1 = st.button("Click Me")
-
-label1 = st.text("This is a label")
-
-if response:
-    label1.text(f"You selected {response} {button1}")
-else:
-    label1.text(f"Please select a choice")
-
-label2 = st.text("This is a label")
-
-button2 = st.button("Click Me1")
-
-if button1:
-    label2.text(f"Button1 Clicked")
-
-if button2:
-    label2.text(f"Button2 Clicked")
-    label2.text(f"Button2 Clicked1")
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    if st.button('Button 1'):
-        st.write('Button 1 clicked')
-
-with col2:
-    if st.button('Button 2'):
-        st.write('Button 2 clicked')
-
-with col3:
-    if st.button('Button 3'):
-        st.write('Button 3 clicked')
+        st.table(data)
---
+++ database_master.py
@@ -0,0 +1,195 @@
+import pyodbc
+import pandas as pd
+
+# Connection strings
+# cn = "DRIVER=SQL Server;SERVER=DESKTOP-DIPRBHN\\MSSQLSERVER1;DATABASE=fyersAlgo;Trusted_Connection=yes;"
+# cn1 = "DRIVER=SQL Server;SERVER=DESKTOP-DIPRBHN\\MSSQLSERVER1;DATABASE=dhanAlgo;Trusted_Connection=yes;"
+# cn2 = 'DRIVER={SQL Server};SERVER=DESKTOP-DIPRBHN\\MSSQLSERVER1;DATABASE=algoMaster;UID=sa;PWD=Lucky;MARS_Connection=Yes'
+cn = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-LKV07EB;DATABASE=fyersAlgo;Trusted_Connection=yes;MARS_Connection=Yes'
+cn1 = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-LKV07EB;DATABASE=dhanAlgo;Trusted_Connection=yes;MARS_Connection=Yes'
+cn2 = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-LKV07EB;DATABASE=algoMaster;Trusted_Connection=yes;MARS_Connection=Yes'
+
+# Functions
+
+def insert_query(sql_query):
+    try:
+        with pyodbc.connect(cn) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            conn.commit()
+    except Exception as e:
+        print("Error:", e)
+
+
+def insert_query_algoMaster(sql_query):
+    try:
+        with pyodbc.connect(cn2) as conn2:
+            cursor2 = conn2.cursor()
+            response = cursor2.execute(sql_query)
+            conn2.commit()
+            return response
+    except Exception as e:
+        print("Error:", e)
+
+
+def insert_query_for_dhan(sql_query):
+    try:
+        with pyodbc.connect(cn1) as conn1:
+            cursor1 = conn1.cursor()
+            cursor1.execute(sql_query)
+            conn1.commit()
+    except Exception as e:
+        print("Error:", e)
+
+
+def get_data(sql_query):
+    try:
+        with pyodbc.connect(cn) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            records = cursor.fetchall()
+            return records
+    except Exception as e:
+        print("Error:", e)
+
+
+def get_data_read_sql(sql_query):
+    try:
+        with pyodbc.connect(cn1) as conn1:
+            df_db = pd.read_sql(sql_query, conn1)
+            return df_db
+    except Exception as e:
+        print("Error:", e)
+
+
+
+def get_data_read_sql_algo(sql_query):
+    try:
+        with pyodbc.connect(cn2) as conn2:
+            df_db = pd.read_sql(sql_query, conn2)
+            return df_db
+    except Exception as e:
+        print("Error:", e)
+
+
+
+def get_data_algoMaster(sql_query):
+    try:
+        with pyodbc.connect(cn2) as conn2:
+            cursor2 = conn2.cursor()
+            cursor2.execute(sql_query)
+            records = cursor2.fetchall()
+            return records
+    except Exception as e:
+        print("Error:", e)
+
+
+def get_data_algoMaster1(sql_query):
+    try:
+        with pyodbc.connect(cn2) as conn2:
+            cursor = conn2.cursor()
+            cursor.execute(sql_query)
+            records = cursor.fetchall()
+            columns = [column[0] for column in cursor.description]
+            return records, columns
+    except Exception as e:
+        print("Error:", e)
+
+
+def get_dhan_data(sql_query):
+    try:
+        with pyodbc.connect(cn1) as conn1:
+            cursor = conn1.cursor()
+            cursor.execute(sql_query)
+            records = cursor.fetchall()
+            return records
+    except Exception as e:
+        print("Error:", e)
+
+
+def delete_table_records(sql_query):
+    try:
+        with pyodbc.connect(cn) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            conn.commit()
+            print("Data deleted successfully")
+    except Exception as e:
+        print("Error:", e)
+
+
+def update_table(sql_query):
+    try:
+        with pyodbc.connect(cn) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            conn.commit()
+    except Exception as e:
+        print("Error:", e)
+
+
+def pcr_form_data(sql_qr):
+    try:
+        with pyodbc.connect(cn) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_qr)
+            records = cursor.fetchall()
+            records_list = [record for record in records]  # Convert to list of tuples
+            return records_list
+    except Exception as e:
+        print("Error:", e)
+
+
+def get_sno(query):
+    sno = 0
+    data = get_data(query)
+    if data:
+        sno = data[0][0]
+    else:
+        sno = 0
+    sno += 1
+    return sno
+
+
+def insert_data_with_parameters(query, params=None):
+    try:
+        with pyodbc.connect(cn2) as conn2:
+            cursor = conn2.cursor()
+            if params:
+                cursor.execute(query, params)
+            else:
+                cursor.execute(query)
+            conn2.commit()
+    except Exception as e:
+        print("Error:", e)
+
+
+def get_data_with_parameters(query, params=None):
+    try:
+        with pyodbc.connect(cn2) as conn2:
+            cursor = conn2.cursor()
+            if params:
+                cursor.execute(query, params)
+            else:
+                cursor.execute(query)
+            return cursor.fetchall()
+    except Exception as e:
+        print("Error:", e)
+
+
+def insert_live_data_using_pandas(insert_query, row):
+    """
+    Function to insert the DataFrame into SQL Server using pyodbc.
+    """
+    try:
+        if row.empty:
+            print("The DataFrame is empty. No data to insert.")
+            return
+
+        row_tuple = tuple(row)
+        with pyodbc.connect(cn2) as conn2:
+            cursor = conn2.cursor()
+            cursor.execute(insert_query, row_tuple)
+            conn2.commit()
+    except Exception as e:
+        print("Error:", e)
